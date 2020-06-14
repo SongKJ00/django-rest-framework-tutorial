12fcddf5d3970055248c820b0acb3dfaa85b94c5
SongKJ00 <gowithdsm@gmail.com>
2020-06-15 00:50:48 +0900

    Chapter 5 - Relationships & Hyperlinked APIs

--- snippets/views.py
+++ snippets/views.py
@@ -6,10 +6,21 @@ from rest_framework.views import APIView
 from rest_framework import mixins
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework import renderers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 from snippets.permissions import IsOwnerOrReadOnly
 from django.contrib.auth.models import User
+
+@api_view(['GET'])
+def api_root(request, format=None):
+  return Response({
+    'users': reverse('user-list', request=request, format=format),
+    'snippets': reverse('snippet-list', request=request, format=format)
+  })
 
 class SnippetList(mixins.ListModelMixin,
                   mixins.CreateModelMixin,
@@ -41,7 +52,7 @@ class SnippetDetail(mixins.RetrieveModelMixin,
   queryset = Snippet.objects.all()
   serializer_class = SnippetSerializer
   permission_classes = [permissions.IsAuthenticatedOrReadOnly,
-                        ]
+                        IsOwnerOrReadOnly]
   def get(self, request, *args, **kwargs):
     return self.retrieve(request, *args, **kwargs)
 
@@ -50,6 +61,14 @@ class SnippetDetail(mixins.RetrieveModelMixin,
 
   def delete(self, request, *args, **kwargs):
     return self.destroy(request, *args, **kwargs)
+
+class SnippetHighlight(generics.GenericAPIView):
+  queryset = Snippet.objects.all()
+  renderer_classes = [renderers.StaticHTMLRenderer]
+
+  def get(self, request, *args, **kwargs):
+    snippet = self.get_object()
+    return Response(snippet.highlighted)
 
 class UserList(generics.ListAPIView):
   queryset = User.objects.all()
